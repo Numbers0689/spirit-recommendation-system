7e46b0a01037639dd8dab61e25c92d8f8d035c14
Numbers0689 <firewatchsai@gmail.com>
2025-02-11 10:54:22 +0530

    Setup project env and streamlit

---
+++ home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Spirit recommendation system")
+
+st.divider()
+
+st.image("https://i.pinimg.com/originals/b3/31/d6/b331d6d35d9de9cd10dc157d72f5c61b.jpg")
+st.subheader("Fill the questionnaire to get your recommendation")
+
+fav_browser = st.text_input("what's your favorite browser?")
+fav_editor = st.text_input("what's your favourite code editor?")
+fav_lang = st.text_input("whats your preferred programming language?")
+fav_movie = st.text_input("whats your favourite movie?")
+fav_drink = st.text_input("whats your go to drink?")
+
+if st.button("submit"):
+    #other stuff
+    st.success("Match has been found!")
+else:
+    st.warning("Please answers all the questions.")
