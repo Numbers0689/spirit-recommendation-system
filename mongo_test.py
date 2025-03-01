e49364ae19ad1a07108d61d91458b09590a2676a
Numbers0689 <firewatchsai@gmail.com>
2025-03-01 15:42:05 +0530

    Remove secrets

--- mongo_test.py
+++ mongo_test.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://numbers0689:99iKtRNNZBjd4t4X@cluster0.499y1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://numbers0689:<db_password>@cluster0.499y1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
